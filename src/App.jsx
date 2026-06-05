2c57609701117d75919caec0e10e7246f16e1b1e
bkemp222 <info@shrimpcabs.com>
2026-06-05 01:14:33 -0400

    shock dig

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const colorways = [
   { id: "bigcat", label: "Big Cat", liveries: ["tiger", "nitro"] },
   { id: "badkitty", label: "Bad Kitty", liveries: ["tiger", "nitro"] },
   { id: "miami", label: "Miami", liveries: ["tiger", "nitro", "shock"] },
-  { id: "dig", label: "Digger", liveries: ["tiger", "nitro"] },
+  { id: "dig", label: "Digger", liveries: ["tiger", "nitro", "shock"] },
   { id: "mania", label: "Mania", liveries: ["tiger", "nitro"] },
   { id: "marley", label: "Irie", liveries: ["shock"] },
   { id: "grateful", label: "Grateful", liveries: ["shock"] },
